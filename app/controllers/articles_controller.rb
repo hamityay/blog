5016f156c63bb531aab67556e00acdc5df9c2283
Hamit YAY <hamityay@hotmail.com>
2016-07-13 21:49:37 +0300

    Fixed Destroy articles issue.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,50 +3,50 @@ class ArticlesController < ApplicationController
 	http_basic_authenticate_with name: "hmt", password:"secret", except: [:index, :show]
 	
 	def index
-   		@articles = Article.all
-  	end
-  
+		@articles = Article.all
+	end
+	
 	def show
-    	@article = Article.find(params[:id])
-  	end
+		@article = Article.find(params[:id])
+	end
 	
 	def new
 		@article = Article.new
 	end
 	
 	def edit
-  		@article = Article.find(params[:id])
+		@article = Article.find(params[:id])
 	end
-
+	
 	def create
-  		@article = Article.new(article_params)
- 
-  		if @article.save
-    			redirect_to @article
-  		else
-    			render 'new'
-  		end
+		@article = Article.new(article_params)
+		
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end
 	end
- 	
+	
 	def update
-  		@article = Article.find(params[:id])
- 
-  		if @article.update(article_params)
-    		redirect_to @article
-  		else
-    		render 'edit'
-  		end
+		@article = Article.find(params[:id])
+		
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
 	end
-
+	
 	def destroy
 		@article = Article.find(params[:id])
 		@article.destroy
-
-		redirected_to articles_path
+		
+		redirect_to articles_path
 	end
-
+	
 	private
-  		def article_params
-    		params.require(:article).permit(:title, :text)
-  		end
+	def article_params
+		params.require(:article).permit(:title, :text)
+	end
 end
